dd6395abdb14691b07af00e4e1455ae30cad5a31
Shahriar Soudeep <s.shahriar32322323@gmail.com>
2025-05-21 15:26:16 +0600

    data ingestion

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-from src.mlProject import logger
+from mlProject import logger
+from mlProject.pipeline.stage_01_data_ingestion_pipeline import DataIngestionTrainingPipeline
 
-logger.info("This is custom log!")
-logger.info("soudeep")
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f">>01 stage{STAGE_NAME} started.........<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>stage{STAGE_NAME} completed.........<<\n")
+except Exception as e:
+    raise e
---
+++ src/mlProject/pipeline/stage_01_data_ingestion_pipeline.py
@@ -0,0 +1,27 @@
+from mlProject.config.cofiguration import ConfigurationManager
+from mlProject.components.data_ingestion_component import DataIngestion
+from mlProject import logger
+
+STAGE_NAME = "Data Ingestion stage"
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
+
+
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>01 stage{STAGE_NAME} started.........<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>stage{STAGE_NAME} completed.........<<\n")
+    except Exception as e:
+        raise e
