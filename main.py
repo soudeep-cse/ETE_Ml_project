a526ab4bf5fbd6aa24cd4497efc7f42a17d5df82
Shahriar Soudeep <83727463+soudeep-cse@users.noreply.github.com>
2025-05-25 23:04:44 +0600

    model trainer added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion_pipeline import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_data_validation_pipeline import DataValidationTrainingPipeline
 from mlProject.pipeline.stage_03_data_transformation_pipeline import DataTransformationTrainingPipeline
-
+from mlProject.pipeline.stage_04_model_trainer_pipeline import ModelTrainerTrainingPipeline
 
 # ----------------------- Data Ingestion -------------------------
 STAGE_NAME = "Data Ingestion stage"
@@ -32,9 +32,21 @@ except Exception as e:
 STAGE_NAME = "Data Transformation stage"
 
 try:
-        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = DataTransformationTrainingPipeline()
-        obj.main()
-        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataTransformationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
+    raise e
+
+# --------------------------- Model Trainer -------------------------
+STAGE_NAME = "Model Trainer stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainerTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
     raise e
